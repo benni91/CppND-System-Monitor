a708a29ff0bbbc286ebf43a604ae5c9790df8c43
Benedikt Menrad <benediktm@posteo.de>
2021-12-28 11:29:47 +0100

    compute cpu utilization

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,12 @@
 #include "processor.h"
+#include "linux_parser.h"
+#include <vector>
+#include <string>
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() { 
+    auto input = LinuxParser::CpuUtilization();
+    int nonIdle = stoi(input[1])+ stoi(input[2])+stoi(input[3])+stoi(input[6])+stoi(input[7]) +stoi(input[8]);
+    int idle = stoi(input[4]) +stoi(input[5]);
+    return float(nonIdle) / float(idle + nonIdle); 
+    }
